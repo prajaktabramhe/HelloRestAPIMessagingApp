a4877a7627100f9127a3ac0e1516f33878f2f314
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-05-17 00:57:21 +0530

    [Prajakta]: Added: Used GET Request Method and used CURL and postman tool to demonstrate the REST API Call

---
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class Controller {
+	
+	@RequestMapping(value = {"","/","/home"})
+	public String sayHello() {
+		return "Hello World Controller";
+	}
+	
+}
