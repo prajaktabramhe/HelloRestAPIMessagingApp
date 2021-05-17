568a6614e304933e1b43bb86368c7ba800d26079
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-05-17 10:58:25 +0530

    [Prajakta]: Added: Used GET Request Method and passed the name as a query parameter and print hello with name

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -1,6 +1,9 @@
 package com.example.demo.controller;
 
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,7 +12,14 @@ public class Controller {
 	
 	@RequestMapping(value = {"","/","/home"})
 	public String sayHello() {
-		return "Hello World Controller";
+		return "Hello From Bridgelabz";
+	}
+	
+	//http://localhost:8080/query?name=Prajakta
+	@RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+	public String sayHello(@RequestParam(value = "name") String name)
+	{
+		return "Hello" + name + "!";
 	}
 	
 }
