3982eb2d4ef9eb4e7e3dffbe51f56fc0664e0009
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-05-17 13:29:54 +0530

    [Prajakta]: Added: Created method for firstname and lastname

---
+++ src/main/java/com/example/demo/model/User/User.java
@@ -0,0 +1,18 @@
+package com.example.demo.model.User;
+
+public class User {
+
+	private String FirstName;
+	private String LastName;
+
+	public String getFirstName() {
+		// TODO Auto-generated method stub
+		return FirstName;
+	}
+
+	public String getLastName() {
+		// TODO Auto-generated method stub
+		return LastName;
+	}
+
+}
