fa10d03efe4fef0fc44e5ec3d9e9bc0476df0544
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-05-17 13:28:32 +0530

    [Prajakta]: Added: Used POST Method and passed first name annd last name

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -3,10 +3,15 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.User.User;
 
 @RestController
 @RequestMapping({"/hello",""})
@@ -25,5 +30,15 @@ public class Controller {
 
 	}
 	
-		
+	@GetMapping("/param/{name}")
+	public String showHello(@PathVariable String name)
+	{
+		return "Hello " + name + "!";
+	}	
+	
+	@PostMapping("/post")
+	public String sayHello(@RequestBody User user)
+	{
+		return "Hello " + user.getFirstName() + " " + user.getLastName() + "!";
+	}
 }
