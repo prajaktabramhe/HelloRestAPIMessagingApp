c44d4c1a286233ef66ee6a38904e6f0b1e4ba7fa
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-05-17 11:54:55 +0530

    [Prajakta]: Added: Used GET Request Method and passed name as a path variable

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -1,13 +1,15 @@
 package com.example.demo.controller;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello")
+@RequestMapping({"/hello",""})
 public class Controller {
 	
 	@RequestMapping(value = {"","/","/home"})
@@ -15,11 +17,17 @@ public class Controller {
 		return "Hello From Bridgelabz";
 	}
 	
-	//http://localhost:8080/query?name=Prajakta
+	//http://localhost:8080/query?name=prajakta
 	@RequestMapping(value = {"/query"}, method = RequestMethod.GET)
 	public String sayHello(@RequestParam(value = "name") String name)
 	{
-		return "Hello" + name + "!";
+		return "Hello " + name + "!";
+	}
+	
+	@GetMapping("/param/{name}")
+	public String showHello(@PathVariable String name)
+	{
+		return "Hello " + name + "!";
 	}
 	
 }
